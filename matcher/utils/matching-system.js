4efc2a5b28e92b4ab8e0fba2db58e398eb00dcf7
Gregory-Hoareau <43096860+Gregory-Hoareau@users.noreply.github.com>
2021-01-27 11:07:16 +0100

    Add Matching Algorithm
    -Add a dummy matching algorithm

--- matcher/utils/matching-system.js
+++ matcher/utils/matching-system.js
@@ -3,6 +3,7 @@ class MatchingSystem {
     constructor () {
         this.driverRequest = []
         this.passengerRequest = []
+        this.isProcessing = false;
     }
 
     addDriverRequest(req) {
@@ -14,7 +15,40 @@ class MatchingSystem {
     }
 
     runMatching() {
-        
+        return new Promise((resolve, reject) => {
+            const matches = [];
+            if(tmpDriver.length === 0 && tmpPassenger.length === 0) resolve(matches)
+            this.isProcessing = true;
+            // Créer une copie des listes des requetes puis vide les listes pour ne pas que 2 promesses traitent les même requetes
+            const tmpDriver = this.driverRequest;
+            const tmpPassenger = this.passengerRequest;
+            this.driverRequest = []
+            this.passengerRequest = [] 
+
+            // TODO - Add matching code strategy
+            let looper;
+            looper = tmpDriver.length > tmpPassenger.length ? tmpDriver : tmpPassenger;
+
+            looper.forEach((val, i) => {
+                matches.push({
+                    'driver': tmpDriver[i],
+                    'passenger': tmpPassenger[i]
+                })
+            })
+
+            // Suppression des requetes qui ont matché
+            matches.forEach(match => {
+                tmpDriver.splice(tmpDriver.indexOf(match.driver), 1);
+                tmpPassenger.splice(tmpPassenger.indexOf(match.passenger), 1);
+            })
+
+            // Rajout des requetes qui n'ont pas matché dans la liste des requetes à traiter
+            tmpDriver.forEach(driverReq => {this.addDriverRequest(driverReq)})
+            tmpPassenger.forEach(passengerReq  => {this.addPassengerRequest(passengerReq)})
+
+            this.isProcessing = false;
+            resolve(matches)
+        })
     }
 }
 
--- matcher/utils/mqtt-service.js
+++ matcher/utils/mqtt-service.js
@@ -2,7 +2,7 @@ const mqtt = require('mqtt');
 
 class MqttService {
     constructor(matchingSystem) {
-        this.mqttBrokerUrl = 'mqtt://<ip>:1883'
+        this.mqttBrokerUrl = 'mqtt://localhost:1883'
         this.clientId = 'ShareMyCarMacther'
         this.client = mqtt.connect(this.mqttBrokerUrl, {clientId: this.clientId})
         this.driverTopic = 'share-my-car-driver'
@@ -17,11 +17,17 @@ class MqttService {
         this.client.on('message', (topic, data) => {
             console.log('Got message')
             if(topic === this.driverTopic) {
-                console.log('RECEIVED : '+ data)
                 this.matcher.addDriverRequest(JSON.parse(data.toString()))
             } else if(topic === this.passengerTopic) {
                 this.matcher.addPassengerRequest(JSON.parse(data.toString()))
             }
+
+            this.matcher.runMatching().then(matches => {
+                matches.forEach(match => {
+                    this.notifyMatch(match);
+                })
+            })
+            
         })
     }
 
@@ -34,6 +40,13 @@ class MqttService {
                 }
             })
         })
+    }
+
+    notifyMatch(match) {
+        const driver = match.driver;
+        const passenger = match.passenger;
+        this.client.publish(passenger.id, JSON.stringify(driver))
+        this.client.publish(driver.id, JSON.stringify(passenger))
     }
 }
 
