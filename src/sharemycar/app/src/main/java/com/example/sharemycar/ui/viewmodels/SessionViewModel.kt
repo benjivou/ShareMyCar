4db0a8b50556f432a2545feb22ca3746426976ed
Gregory-Hoareau <43096860+Gregory-Hoareau@users.noreply.github.com>
2021-01-31 15:33:24 +0100

    Add the thanking pages

--- src/sharemycar/app/src/main/java/com/example/sharemycar/ui/viewmodels/SessionViewModel.kt
+++ src/sharemycar/app/src/main/java/com/example/sharemycar/ui/viewmodels/SessionViewModel.kt
@@ -6,4 +6,5 @@ import com.example.sharemycar.data.models.User
 
 class SessionViewModel :ViewModel() {
     val user: MutableLiveData<User?> = MutableLiveData(null) // current user loaded
+    val isDriver: MutableLiveData<Boolean> = MutableLiveData(false)
 }
