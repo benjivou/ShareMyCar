52b77d1a0446e9eaa64fc920a93fb99e72f70b5b
Gregory-Hoareau <43096860+Gregory-Hoareau@users.noreply.github.com>
2021-02-01 00:09:04 +0100

    Add match fragment and its view model

---
+++ src/sharemycar/app/src/main/java/com/example/sharemycar/ui/fragment/MatchFragment.kt
@@ -0,0 +1,58 @@
+package com.example.sharemycar.ui.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.sharemycar.R
+import com.example.sharemycar.databinding.MatchFragmentBinding
+import com.example.sharemycar.ui.viewmodels.MatchViewModel
+import com.example.sharemycar.ui.viewmodels.SessionViewModel
+
+class MatchFragment : Fragment() {
+
+    private var _binding : MatchFragmentBinding? = null
+    private val binding get() = _binding!!;
+
+    private val viewModel: MatchViewModel by viewModels()
+    private val sessionViewModel: SessionViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MatchFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.acceptBtn.setOnClickListener {
+
+        }
+        binding.refuseBtn.setOnClickListener {
+
+        }
+    }
+
+    fun receivedNewMatch() {
+        sessionViewModel.isDriver.observe(viewLifecycleOwner, Observer {
+            if(it) {
+                viewModel.driver.observe(viewLifecycleOwner, Observer { user ->
+                    binding.userName.text = user.username
+                })
+            } else {
+                viewModel.passenger.observe(viewLifecycleOwner, Observer { user ->
+                    binding.userName.text = user.username
+                })
+            }
+        })
+    }
+
+}
