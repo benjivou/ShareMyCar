3612e9388b590f67bf7d8379e0d22cec891117a5
Gregory-Hoareau <43096860+Gregory-Hoareau@users.noreply.github.com>
2021-02-02 15:12:24 +0100

    Re add line for maps key

--- src/sharemycar/app/src/main/java/com/example/sharemycar/App.kt
+++ src/sharemycar/app/src/main/java/com/example/sharemycar/App.kt
@@ -1,16 +1,16 @@
 package com.example.sharemycar
 
 import android.app.Application
-//import com.example.sharemycar.data.untracked.PLACES_KEY
+import com.example.sharemycar.data.untracked.PLACES_KEY
 import com.google.android.libraries.places.api.Places
 
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
         // Initialize the SDK
-        //Places.initialize(applicationContext, PLACES_KEY)
+        Places.initialize(applicationContext, PLACES_KEY)
 
         // Create a new PlacesClient instance
-        //val placesClient = Places.createClient(this)
+        val placesClient = Places.createClient(this)
     }
 }
